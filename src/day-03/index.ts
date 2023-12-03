78eef7e6232e7e485b2b73f0f87e077388de0d5d
Your Name <you@example.com>
2023-12-03 18:57:14 +0100

    part 2 for day 3

--- src/day-03/index.ts
+++ src/day-03/index.ts
@@ -5,13 +5,116 @@ interface Square {
   isInt: boolean;
 }
 
+function setAdjacencyForIncrements(
+  row: number,
+  col: number,
+  incrementList: number[][],
+  newEntry: number,
+  adjacencyMap: Record<number, Record<number, number[]>>,
+): void {
+  incrementList.forEach(([rowIncremenet, colIncrement]) => {
+    if (!adjacencyMap[row + rowIncremenet]) {
+      adjacencyMap[row + rowIncremenet] = {};
+    }
+
+    if (!adjacencyMap[row + rowIncremenet][col + colIncrement]) {
+      adjacencyMap[row + rowIncremenet][col + colIncrement] = [];
+    }
+
+    adjacencyMap[row + rowIncremenet][col + colIncrement].push(newEntry);
+  });
+}
+
+function setAdjacency(
+  row: number,
+  currentNumberString: string,
+  currentNumberColumns: number[],
+  adjacencyMap: Record<number, Record<number, number[]>>,
+): void {
+  setAdjacencyForIncrements(
+    row,
+    currentNumberColumns[0],
+    [
+      [-1, -1],
+      [0, -1],
+      [1, -1],
+    ],
+    +currentNumberString,
+    adjacencyMap,
+  );
+  currentNumberColumns.forEach((numberCol: number) =>
+    setAdjacencyForIncrements(
+      row,
+      numberCol,
+      [
+        [-1, 0],
+        [1, 0],
+      ],
+      +currentNumberString,
+      adjacencyMap,
+    ),
+  );
+  setAdjacencyForIncrements(
+    row,
+    currentNumberColumns.at(-1),
+    [
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+    ],
+    +currentNumberString,
+    adjacencyMap,
+  );
+}
+
+function parseRow2(
+  line: string,
+  row: number,
+  adjacencyMap: Record<number, Record<number, number[]>>,
+): Square[] {
+  let currentNumberString: string = "";
+  let currentNumberColumns: number[] = [];
+
+  return line.split("").map((char: string, col: number, arr: string[]) => {
+    const isInt: boolean = !isNaN(+char);
+
+    if (isInt) {
+      currentNumberString += char;
+      currentNumberColumns.push(col);
+
+      if (col === arr.length - 1) {
+        setAdjacency(
+          row,
+          currentNumberString,
+          currentNumberColumns,
+          adjacencyMap,
+        );
+      }
+    } else {
+      setAdjacency(
+        row,
+        currentNumberString,
+        currentNumberColumns,
+        adjacencyMap,
+      );
+      currentNumberString = "";
+      currentNumberColumns = [];
+    }
+
+    return {
+      value: char,
+      isInt,
+    };
+  });
+}
+
 function parseRow(
   line: string,
   row: number,
   adjacencyMap: Record<number, Record<number, boolean>>,
 ): Square[] {
   return line.split("").map((char: string, cell: number) => {
-    const isInt: boolean = !isNaN(parseInt(char));
+    const isInt: boolean = !isNaN(+char);
 
     const isSymbol: boolean = !isInt && char !== ".";
     if (isSymbol) {
@@ -81,8 +184,6 @@ function getAdjcentNumbers(filename: string): number[] {
     }
   }
 
-  console.log(numbers);
-
   return numbers;
 }
 
@@ -90,6 +191,37 @@ function sumAdjacentNumbers(filename: string): number {
   return getAdjcentNumbers(filename).reduce((a, b) => a + b, 0);
 }
 
+function getGearNumbers(filename: string): number[] {
+  const adjacencyMap: Record<number, Record<number, number[]>> = {};
+  const grid: Square[][] = FileUtils.mapFileToList(
+    __dirname,
+    filename,
+    (line: string, index: number) => parseRow2(line, index, adjacencyMap),
+  );
+
+  const numbers: number[] = [];
+
+  for (let i = 0; i < grid.length; i++) {
+    const row: Square[] = grid[i];
+    for (let j = 0; j < row.length; j++) {
+      const cell: Square = row[j];
+      if (cell.value === "*" && adjacencyMap[i]?.[j]?.length === 2) {
+        numbers.push(adjacencyMap[i][j][0] * adjacencyMap[i][j][1]);
+      }
+    }
+  }
+
+  return numbers;
+}
+
+function sumGearNumbers(filename: string): number {
+  return getGearNumbers(filename).reduce((a, b) => a + b, 0);
+}
+
 console.log("Example Part 1: ", sumAdjacentNumbers("./example.txt"));
 
 console.log("Part 1: ", sumAdjacentNumbers("./input.txt"));
+
+console.log("Example Part 2: ", sumGearNumbers("./example.txt"));
+
+console.log("Part 2: ", sumGearNumbers("./input.txt"));
